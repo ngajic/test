54ba650cdb62b9dd87da4052d4568ad60b5dc0ec
Nikola <ngajic@geneko.rs>
2018-01-19 10:59:24 +0100

    Function isPrime()

--- utils.c
+++ utils.c
@@ -1,11 +1,31 @@
 #include <stdio.h>
 #include <string.h>
+#include <math.h>
 #include <utils.h>
 
 /**
+* @brief Checks if it is a prime number.
+* This function checks if associated number is a prime number. Which means it
+* does not have any positive divisor besides 1 and itself.
+*
+* @param[in] num is a variable where number being checked is stored;
+* @return 1 if number is a prime, 0 otherwise!
+*/
+char isPrime(long num)
+{
+	long i;
+	if((num <= 0) || (num%2 == 0))
+		return 0;
+	for(i = 3; i <= (int)floor(sqrt(num)); i += 2)
+		if(num%i == 0)
+			return 0;
+	return 1;
+}
+
+/**
 * @brief Returns array period.
-* That is, size of an inner array which can be repeated multiple times in order
-* to get starting \e array.
+* This function returns size of an inner array which can be repeated multiple
+* times in order to get starting \e array.
 *
 * @param[in] array is a pointer to starting array.
 * @param[in] size is the size of starting array.
--- utils.h
+++ utils.h
@@ -1,6 +1,7 @@
 #ifndef _UTIL_H
 #define _UTIL_H
 
+char isPrime(long);
 int arrayPeriod(int*, int);
 void printBytes(void* , unsigned short);
 
